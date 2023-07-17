bd0839b883c5ebea3aa699c1bee2a6c741b3c744
yangdi <di.yang@hongjingdrive.com>
2023-07-17 08:30:05 +0800

    删除编译中间文件和可执行文件

--- pub-sub/build/CMakeFiles/zmq_comm.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for zmq_comm.
-# This may be replaced when dependencies are built.
--- zmq_interactor/build_x86/src/CMakeFiles/zmq_interactor.dir/mock_zmq_publiser.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/zmq_interactor.dir/mock_zmq_publiser.cpp.o: \
- /home/yangdi/work/hpp/autodrive-data-buried/mock/zmq_interactor/src/mock_zmq_publiser.cpp \
- /usr/include/stdc-predef.h
